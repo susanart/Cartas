c0226c153c2021579a7a9b094ad3c10de9d0170f
susanart <susanart@ufps.edu.co>
2021-04-25 11:55:44 -0500

    agg js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
+window.onload = () => {
+  this.sessionStorage.setItem("usuario","usuario")
+  this.sessionStorage.setItem("password","12345")
+}
+
+//variables 
+let input = document.getElementsByTagName('input')
+let login = document.getElementById('login')
+let form = document.querySelector('form')
+
+
+
+
+
 async function leerJSON(url) {
     try {
       let response = await fetch(url);

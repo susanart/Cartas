d4a9a4b4b1ccc124c19a217b54954eed7f2822c1
susanart <susanart@ufps.edu.co>
2021-04-25 13:13:26 -0500

    login

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,78 @@
+// async function leerJSON(url) {
+//   try {
+//     let response = await fetch(url);
+//     let user = await response.json();
+//     return user;
+//   } catch (err) {
+//     alert(err);
+//   }
+// }
+
+// function mostrar() {
+//   var url = "https://raw.githubusercontent.com/susanart/Cartas/main/cartas.json";
+//   var table = "";
+//       leerJSON(url).then((cartas) => {
+//       cartas.forEach((cartas1) => {
+        
+//           table += `
+//               <tr>
+//                 <td>${cartas1.numero}</td>
+//                 <td>${cartas1.carta}</td>
+//                 <td>${cartas1.cantidad}</td>
+//               </tr>
+           
+//           `;
+//       });
+//       document.getElementById("contenido").innerHTML = table;
+//     })
+// }
+// mostrar();
+
 window.onload = () => {
-  this.sessionStorage.setItem("usuario","usuario")
-  this.sessionStorage.setItem("password","12345")
+  this.sessionStorage.setItem("usuario","usuario");
+  this.sessionStorage.setItem("password","12345");
 }
 
 //variables 
-let input = document.getElementsByTagName('input')
-let login = document.getElementById('login')
-let form = document.querySelector('form')
+var input = document.getElementsByTagName('input')
+var login = document.getElementById('login')
+var form = document.querySelector('form')
 
+form.onsubmit = () => {return false}
 
+login.onclick = () => {
+  if((input[0].value != "") && (input[1].value != "")){
+    if((input[0].value == sessionStorage.getItem("usuario")) && (input[1].value == sessionStorage.getItem("password"))){
+      form.onsubmit = () => {return 1}
+    } else{
+      if (input[0].value != sessionStorage.getItem("usuario")){
+        input[0].nextElementSibling.textContent = "Usuario incorrecto";
+        setTimeout(()=>{
+          input[0].nextElementSibling.textContent ="";
+        },2000);
+      }
+      if (input[1].value != sessionStorage.getItem("password")){
+        input[1].nextElementSibling.textContent = "Contraseña incorrecta";
+        setTimeout(()=>{
+          input[1].nextElementSibling.textContent ="";
+        },2000);
+      }
+    }
 
-
-
-async function leerJSON(url) {
-    try {
-      let response = await fetch(url);
-      let user = await response.json();
-      return user;
-    } catch (err) {
-      alert(err);
+  } else{
+    if(input[0].value == ""){
+        input[0].nextElementSibling.textContent = "Digite Usuario";
+        setTimeout(()=>{
+          input[0].nextElementSibling.textContent ="";
+        },2000);
+    }
+    if(input[1].value == ""){
+        input[1].nextElementSibling.textContent = "Digite Contraseña";
+        setTimeout(()=>{
+          input[1].nextElementSibling.textContent ="";
+        },2000);
     }
   }
-
-  function mostrar() {
-    var url = "https://raw.githubusercontent.com/susanart/Cartas/main/cartas.json";
-    var table = "";
-        leerJSON(url).then((cartas) => {
-        cartas.forEach((cartas1) => {
-          
-            table += `
-                <tr>
-                  <td>${cartas1.numero}</td>
-                  <td>${cartas1.carta}</td>
-                  <td>${cartas1.cantidad}</td>
-                </tr>
-             
-            `;
-        });
-        document.getElementById("contenido").innerHTML = table;
-      })
-  }
-
-  mostrar();
+}
 
 
-
-  
